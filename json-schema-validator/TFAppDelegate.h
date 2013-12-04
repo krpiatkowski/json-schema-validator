716a8328ebb45971a4b443b27972893ab123f876
Krzysztof Piatkowski <kpi@trifork.com>
2013-12-04 11:01:22 +0100

    Initial xcode commit

---
+++ json-schema-validator/TFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TFAppDelegate.h
+//  json-schema-validator
+//
+//  Created by Krzysztof Piatkowski on 04/12/13.
+//  Copyright (c) 2013 Trifork. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ json-schema-validator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  json-schema-validator
+//
+//  Created by Krzysztof Piatkowski on 04/12/13.
+//  Copyright (c) 2013 Trifork. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TFAppDelegate class]));
+    }
+}
